da987c3fa90303e529807bce442f5e6c38ec0b43
Karl Hendrik Leppmets <spruur@gmail.com>
2018-05-22 17:53:48 +0300

    Added new note creation functionality

--- src/notes/Main.java
+++ src/notes/Main.java
@@ -3,10 +3,12 @@ package notes;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.sql.*;
@@ -31,6 +33,9 @@ public class Main extends Application {
     private TextArea textArea = new TextArea();
     private ListView<Note> listView;
     private Note currentNote;
+    private TextField newNoteTitle;
+    private Stage dialog = new Stage();
+    private ObservableList<Note> notesList = FXCollections.observableArrayList();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -88,30 +93,51 @@ public class Main extends Application {
     }
 
     private void buildView() throws Exception {
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(window);
         window.setWidth(500);
         BorderPane bPane = new BorderPane();
 
-        ObservableList<Note> data = FXCollections.observableArrayList();
-
-        listView = new ListView<>(data);
+        listView = new ListView<>(notesList);
         listView.setPrefSize(200, window.getHeight());
         listView.setMaxWidth(200);
         listView.setMinWidth(100);
         listView.setLayoutX(0);
         listView.setLayoutY(0);
 
-        data.addAll(notes);
+        notesList.addAll(notes);
 
-        listView.setItems(data);
+        listView.setItems(notesList);
         bPane.setLeft(listView);
 
-        textArea.setText("Testilause");
+        textArea.setText("This simple note app is built by Karl Hendrik\nLeppmets as a Java project for Java\nTechnologies course in TTÜ IT College.");
         textArea.setMinWidth(200);
         textArea.setPrefWidth(300);
         textArea.setMaxWidth(1000000);
         textArea.setWrapText(true);
 
         bPane.setRight(textArea);
+
+        HBox hbox = new HBox();
+        hbox.setMinWidth(300);
+        hbox.setPrefWidth(500);
+        hbox.setMaxWidth(1000000);
+        hbox.setPadding(new Insets(15, 12, 15, 12));
+        hbox.setSpacing(10);
+        hbox.setStyle("-fx-background-color: #336699;");
+
+        Button newButton = new Button("New note");
+        newButton.setPrefSize(100, 20);
+
+        Button saveButton = new Button("Save");
+        saveButton.setPrefSize(100, 20);
+
+        Pane spacer = new Pane();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        spacer.setMinSize(10, 1);
+
+        hbox.getChildren().addAll(newButton, spacer, saveButton);
+        bPane.setBottom(hbox);
 
         // Listeners
         bPane.widthProperty().addListener((ChangeListener) -> textArea.setPrefWidth(window.getWidth() - listView.getWidth()));
@@ -124,24 +150,38 @@ public class Main extends Application {
             }
         });
         textArea.textProperty().addListener((observable, oldValue, newValue) -> {textArea.setPrefWidth(window.getWidth() - listView.getWidth()); textEdited(); });
+        saveButton.setOnAction(e -> {
+            try {
+                saveCurrentNote();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
 
+        newButton.setOnAction(e -> {
+            try {
+                openTitleModal();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
 
 
         window.setTitle("Notes by Spruur");
         window.setScene(new Scene(bPane, 500, 400));
-        window.setMinWidth(300);
+        window.setMinWidth(400);
         window.setMinHeight(200);
         window.show();
     }
 
     private void textEdited() {
-        currentNote.updateContent(textArea.getText());
+        if (currentNote != null)
+            currentNote.updateContent(textArea.getText());
     }
 
     private void setCurrentNote(Note note) throws Exception {
         saveCurrentNote();
         textArea.setPrefWidth(window.getWidth() - listView.getWidth());  // Just in case for screen size fixing.
-
 
         currentNote = note;
         textArea.setText(currentNote.getContent());
@@ -156,8 +196,55 @@ public class Main extends Application {
         }
     }
 
+    private void openTitleModal() throws Exception {
+        VBox dialogVbox = new VBox(20);
 
+        Label label = new Label("Title for new note: ");
+        label.setMinWidth(300);
+        label.setAlignment(Pos.CENTER);
+        newNoteTitle = new TextField();
 
+        Button createNotebutton = new Button("Create new note");
+        createNotebutton.setAlignment(Pos.CENTER);
+
+        createNotebutton.setOnAction(e -> {
+            try {
+                newNote();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        dialogVbox.getChildren().addAll(label, newNoteTitle, createNotebutton);
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
+    }
+
+    private void newNote() throws Exception{
+        PreparedStatement stmt = db.prepareStatement("INSERT INTO "+dbPrefix+"notes (title, content, edited) VALUES (?, '', NOW())", Statement.RETURN_GENERATED_KEYS);
+        stmt.setString(1, newNoteTitle.getText().length() > 0 ? newNoteTitle.getText() : null);
+        stmt.executeUpdate();
+        ResultSet keys = stmt.getGeneratedKeys();
+        int key;
+        if (keys.next()) {
+            key = keys.getInt(1);
+        }
+        else {
+            throw new Exception("No data inserted!");
+        }
+
+        Statement stmt2 = db.createStatement();
+        ResultSet rs = stmt2.executeQuery("SELECT * FROM "+dbPrefix+"notes WHERE id="+key);
+        if (rs.next()) {
+            currentNote = new Note(rs);
+            dialog.hide();
+            textArea.setText(currentNote.getContent());
+            notesList.add(currentNote);
+        }
+        else
+            throw new Exception("No new data to get!");
+    }
 
     public static void main(String[] args) {
         launch(args);
